ccb747758e38bc13e854e7357a2aaf6ead86d868
wangyihu2009@msn.com <wangyihu2009@msn.com>
2020-08-04 15:04:57 +0800

    first commit

---
+++ ReflectionDemo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ReflectionDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Models.NormalClass normalClass = new Models.NormalClass();
+            //normalClass[0] = "abcd";
+            //Console.WriteLine(normalClass[0]);
+
+            //Console.WriteLine("Hello World!");
+            //FlagEnumDemo();
+
+            Samples.Sample1.Demo9();
+        }
+
+        static void FlagEnumDemo()
+        {
+            Models.FlagEnum flagEnum = Models.FlagEnum.Three | Models.FlagEnum.Five;
+            Console.WriteLine(flagEnum.HasFlag(Models.FlagEnum.Three));
+            Console.WriteLine(flagEnum.HasFlag(Models.FlagEnum.One));
+
+            Models.NormalEnum normalEnum = Models.NormalEnum.One | Models.NormalEnum.Four;
+            Console.WriteLine(normalEnum.HasFlag(Models.NormalEnum.Three));
+            Console.WriteLine(normalEnum.HasFlag(Models.NormalEnum.One));
+        }
+    }
+}
